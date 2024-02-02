ef6b12dc8283a6a7a3229f4adbda2cb3f03167f7
Pedro3DS <pedroca2787@gmail.com>
2024-02-01 21:39:48 -0400

    update

--- Assets/Script/DatabaseManager.cs
+++ Assets/Script/DatabaseManager.cs
@@ -1,18 +1,29 @@
+using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DatabaseManager : MonoBehaviour
 {
+    public TMP_InputField Name;
+    public TMP_InputField Email;
+    public TMP_InputField Password;
+
+    private DatabaseReference dbRederence;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dbRederence = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateUser()
     {
-        
+        User newUser = new User(Name.text, Email.text, Password.text);
+        string json = JsonUtility.ToJson(newUser);
+
+        dbRederence.Child("users").Child(Email.text).SetRawJsonValueAsync(json);
     }
 }
