6c7fadf9dbcca36cc544bbef3a5a69b7130195b6
Pedro3DS <pedroca2787@gmail.com>
2024-02-07 21:11:45 -0400

    update

--- Assets/Scripts/EmployeeScript.cs
+++ Assets/Scripts/EmployeeScript.cs
@@ -6,19 +6,7 @@ using UnityEngine.UIElements;
 
 public class EmployeeScript : MonoBehaviour
 {
-    //Buttons
-    public GameObject homeButton;
-    public GameObject managementButton;
-    public GameObject listButton;
-    public GameObject employeesButton;
-    public GameObject userButton;
 
-    //Activated Buttons Sprites
-    public GameObject homeButtonActivated;
-    public GameObject listButtonActivated;
-    public GameObject managementButtonActivated;
-    public GameObject employeesButtonActivated;
-    public GameObject userButtonActivated;
 
     private string userCpf;
     void Start()
@@ -27,19 +15,4 @@ public class EmployeeScript : MonoBehaviour
     }
 
 
-    public void actuvatedButtonEffect(string btnName)
-    {
-        Debug.Log("ç,çl,çl");
-        switch (btnName)
-        {
-            case "home":
-                homeButton.SetActive(false);
-                homeButtonActivated.SetActive(true);
-                break;
-            default:
-                Debug.Log("sdfghjk");
-                break;
-        }
-            
-    }
 }
