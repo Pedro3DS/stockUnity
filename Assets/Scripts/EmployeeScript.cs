465f519e030259204bc07e2cbbdf86ee6b509d51
Pedro3DS <pedroca2787@gmail.com>
2024-02-11 16:58:12 -0400

    update

--- Assets/Scripts/EmployeeScript.cs
+++ Assets/Scripts/EmployeeScript.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using TMPro;
 using System;
+using UnityEngine.Networking;
 
 public class EmployeeScript : MonoBehaviour
 {
-    public TMP_Text userName;
-    public TMP_Text userEmail;
+    public TMP_InputField userName;
+    public TMP_InputField userEmail;
     public TMP_Text userHierarchy;
     public RawImage userImg;
 
@@ -21,20 +22,39 @@ public class EmployeeScript : MonoBehaviour
 
     async void Start()
     {
-        Debug.Log("hbhbhjbhj");
         db = new DatabaseManager();
         db.Start();
 
         userSnapshot = await db.getUserData(cpfT);
-        Debug.Log("aaaaaaaaaaaaaa");
     }
 
     public void setUserInformations()
     {
+        userImg = gameObject.GetComponent<RawImage>();
         userName.text = userSnapshot.Child("Name").Value.ToString();
         userEmail.text = userSnapshot.Child("Email").Value.ToString();
-        userHierarchy.text = userSnapshot.Child("Hierarchy").Value.ToString();
+        if (userSnapshot.Child("Hierarchy").Value.ToString() == "employee")
+        {
+            userHierarchy.text = "Funcionario Normal";
+        }
+        StartCoroutine(setUserPhotoProfile());
+        
+    }
 
+    private IEnumerator setUserPhotoProfile()
+    {
+        Debug.Log(userSnapshot.Child("ProfilePhoto").Value.ToString());
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture("https://firebasestorage.googleapis.com/v0/b/stockunity-46765.appspot.com/o/" + userSnapshot.Child("ProfilePhoto").Value.ToString() + "?alt=media&token=");
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            userImg.texture = texture;
+        }
     }
 
 }
